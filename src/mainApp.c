b1270f28aab023dd7934e05befeb0643f736d1df
Doron Shapiro <shapiro.doron@gmail.com>
2012-01-04 23:36:13 -0500

    Added main program file to fork into other functions

---
+++ src/mainApp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//I will be the user interactions process
+//First, I will fork off a canvas and client
+//Then I'll package  mouse events/movements for the rest of my life
+
+int main(){
+  int canvas, client;
+  canvas = fork();
+  client = fork();
+}
