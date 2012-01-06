c9ff06ae7ee9ee6cad7bcc8a2b4859e9c738823e
Doron Shapiro <shapiro.doron@gmail.com>
2012-01-05 23:00:05 -0500

    Working main program outline with dummy functions

--- src/defs.h
+++ src/defs.h
@@ -4,6 +4,9 @@
 #include <math.h>
 #include "SDL/SDL.h"
 
+#define READ 0
+#define WRITE 1
+
 typedef struct Mouse {
     int xcor;
     int ycor;
--- src/mainApp.c
+++ src/mainApp.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
 
 //I will be the user interactions process
 //First, I will fork off a canvas and client
 //Then I'll package  mouse events/movements for the rest of my life
 
+void dummyCanvasFunction(){}
+void dummyClientFunction(){}
+void dummyInteractionsFunction(){}
+
 int main(){
   int canvas, client;
+  int canvasPipe[2], clientPipe[2];
+  pipe(canvasPipe);
+  pipe(clientPipe);
+
   canvas = fork();
   client = fork();
+
+  if(canvas == 0)
+    dummyCanvasFunction();
+  else if(client == 0)
+    dummyClientFunction();
+  else
+    dummyInteractionsFunction();
+  return 1;
 }
