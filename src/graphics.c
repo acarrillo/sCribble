2f2837e95e6dd2d63f76a6b0521709a577872417
Alejandro Carrillo <aecarrillo594@gmail.com>
2012-01-09 20:31:03 -0500

    Remove trailing whitespace

--- src/graphics.c
+++ src/graphics.c
@@ -76,7 +76,7 @@ void updateScreen() {
         mouse.lastx = mouse.xcor;
         mouse.lasty = mouse.ycor;
     }
-    
+
     //if the mouse is under the divider line, then it will take in a new color.
     //if the mouse is on the black button, it will reset the RGB values.
     else if(mouse.ycor > 450 && mouse.xcor <= 25){
@@ -86,11 +86,11 @@ void updateScreen() {
     }
     //change R values by intervals of 50.
     else if(mouse.ycor > 450 && mouse.xcor <= 150){
-      color.r = (Uint8)mouse.xcor / 25 * 50;   
+      color.r = (Uint8)mouse.xcor / 25 * 50;
     }
     //change G values by intervals of 50.
     else if(mouse.ycor >450 && mouse.xcor <= 275){
-      //something went wrong with this so I hardcoded the issue out.	
+      //something went wrong with this so I hardcoded the issue out.
       if(mouse.xcor > 250 && mouse.xcor <=275){
 	color.g=250;
       }
@@ -103,14 +103,14 @@ void updateScreen() {
       color.b=(Uint8)mouse.xcor / 25 * 50;
     }
     //sets the pen to "erase" (just painting the page white emulates erasing)
-    else if(mouse.ycor > 425){ 
+    else if(mouse.ycor > 425){
       color.r=255;
       color.g=255;
       color.b=255;
     }
 
     //divider line separates drawing space and the UI space.
-    drawLine(0, 450, 650, 450, 0, 0, 0); 
+    drawLine(0, 450, 650, 450, 0, 0, 0);
 
     //draw color indicators.
     drawFilledRect(0,450,15,25, 0,0,0);
@@ -129,7 +129,7 @@ void updateScreen() {
     drawFilledRect(325,450,15, 25,0,0,150);
     drawFilledRect(350,450,15, 25,0,0,200);
     drawFilledRect(375,450,15, 25,0,0,250);
-    
+
     //tried to make it easier. for some reason, it didn't work.
     /*
     int xclr;
@@ -150,7 +150,7 @@ void updateScreen() {
     */
 
     //indicates the current color of the pen.
-    drawFilledRect(500,450,15, 15,color.r, color.g, color.b); 
+    drawFilledRect(500,450,15, 15,color.r, color.g, color.b);
 
 
     if(SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen); //Unlocks surface, done writing
