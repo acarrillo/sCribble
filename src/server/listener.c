abcc7fdd6c234fa81b5e0ee0ce529b5d27313359
Doron Shapiro <shapiro.doron@gmail.com>
2012-01-22 11:48:02 -0500

    [UNTESTED] All server functionality written

---
+++ src/server/listener.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "server.h"
+#include "subserver.h"
+
+int socket_id;
+struct sockaddr_in server;
+
+void server_listener_setup(){
+  int i;
+
+  //make the server socket for reliable IPv4 traffic
+  socket_id = socket( AF_INET, SOCK_STREAM, 0);
+  printf("Socket file descriptor: %d\n", socket_id);
+
+  //set up the server socket struct (use IPv4)
+  server.sin_family = AF_INET;
+
+  //This is the server, so it will listen to anything coming into the host computer
+  server.sin_addr.s_addr = INADDR_ANY;
+  
+  //set the port to listen on, htons converts the port number to network format
+  server.sin_port = htons(44444);
+  
+  //bind the socket to the socket struct
+  i = bind( socket_id, (struct sockaddr *)&server, sizeof(server) );
+
+  //wait for any connection
+  i = listen( socket_id, 0 );    
+}
+
+int add_client(int sock_client){
+  int i;
+  for(i=0;i<64; i++)
+    if(!clientList[i]){
+      clientList[i] = sock_client;
+      break;
+    }
+  return 64 - i;
+}
+
+void server_listener(){
+  int socket_client, b, subserver;
+  struct cribblePacket buffer;
+  socklen_t socket_length;
+
+  server_listener_setup();
+  while(1) {
+    printf("Waiting for a connection\n");
+
+    //set socket_length after the connection is made
+    socket_length = sizeof(server);
+    socklen_t l = sizeof(server);
+
+    //accept the incoming connection, create a new file desciptor for the socket to the client | BLOCKS
+    socket_client = accept(socket_id, (struct sockaddr *)&server, &l);
+
+    printf("accepted connection %d\n",socket_client);
+
+    b = read( socket_client, &buffer, sizeof(buffer) );
+    if(buffer.type == C_CONNECT){
+      add_client(socket_client);
+      subserver = fork();
+      if(subserver == 0)
+      	subserve(socket_client);
+    }
+  }
+}
--- src/server/server.h
+++ src/server/server.h
@@ -4,7 +4,7 @@
 #define MESSAGE_POT_KEY 727422536
 #define MESSAGE_POT_SEM 727422537
 
-#define POT_FULL 2
+#define POT_FULL 3
 #define POT_ADDING 1
 #define POT_EMPTY 0
 
