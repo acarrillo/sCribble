0e2531047f275cbffafee1c74cf30ef134f16765
Doron Shapiro <shapiro.doron@gmail.com>
2012-01-21 22:04:32 -0500

    [UNTESTED] Completed main and "talker" server

---
+++ src/server/server.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <stdint.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#define AM_SERVER 1
+#include "talker.h"
+#include "server.h"
+
+/* FROM server.h : 
+  int semid;
+  cribblePacket* messagePot;
+  int* clientList;
+*/
+
+int main(int argc, char *argv[]){
+  int clientListDescriptor, talker, messagePotDescriptor;
+  union semun semop;
+  
+  //Set up a list of client socket descriptors in shared memory
+  clientListDescriptor = shmget(CLIENT_LIST_KEY, 64*sizeof(int), 0664 | IPC_CREAT | IPC_EXCL);
+  clientList = (int*) shmat(clientListDescriptor, NULL, 0);
+  
+  //Set up the messagePot in shared memory
+  messagePotDescriptor = shmget(MESSAGE_POT_KEY, 64*sizeof(int), 0664 | IPC_CREAT | IPC_EXCL);
+  messagePot = (cribblePacket*) shmat(messagePotDescriptor, NULL, 0);  
+
+  //Set up a semaphore for the messagePot
+  semid = semget(MESSAGE_POT_SEM, 1, 0664 | IPC_CREAT | IPC_EXCL);
+  semop.val = POT_EMPTY;
+  semctl(semid, 0, SETVAL, semop);
+
+  talker = fork();
+   if(talker == 0)
+     server_talker(CLIENT_LIST_KEY);
+   //else
+  //server_listener();
+  
+  //remove shared memory
+  shmdt(clientList);
+  shmdt(messagePot);
+
+  return 1;
+}
---
+++ src/server/talker.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#include "server.h"
+
+void blastMessage(char* message){
+  cribblePacket buffer;
+  buffer.type = C_STATUS;
+  strcpy(buffer.data, message);
+  int i;
+  for(i=0; i<64; i++)
+    write( clientList[i], &buffer, sizeof(buffer));
+}
+
+void removeClient(int sock_client){
+  //Remove client from the clients list
+  int i;
+  for(i=0;i<64; i++)
+    if(clientList[i] == sock_client)
+      clientList[i] = 0;
+  //Tell everyone about it
+    char message[140];
+    strcpy(message, messagePot->data);
+    strcat(message, " left the doc");
+    blastMessage(message);
+}
+
+void server_talker(int clientListKey){
+  struct sembuf sop;
+  sop.sem_num = 0;
+  sop.sem_flg = 0;
+
+  while(1){
+    //Block until the pot semaphore is ready
+    sop.sem_op = -2;
+    semop(semid, &sop, 1);
+    if(messagePot->type == C_DISCONNECT)
+      removeClient(atoi(messagePot->data)); //Disconnect messages already have the client descriptor inserted into their message
+    else {
+      int i;
+      //   strcat(data.data,"SERVER");
+      for(i=0; i<64; i++)
+	write( clientList[i], &messagePot, sizeof(messagePot));
+    }
+    sop.sem_op = 2;
+    semop(semid, &sop, 1);
+  }
+}
