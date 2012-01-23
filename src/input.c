0b78768b24c55d413b5c621431d5fe91b5978371
azureity <don_vinny@yahoo.com>
2012-01-23 13:44:33 -0500

    Update src/input.c

--- src/input.c
+++ src/input.c
@@ -36,6 +36,13 @@ void getInput()
                         if (event.button.state == SDL_PRESSED) {
                             mouse.xcor = event.button.x;
                             mouse.ycor = event.button.y;
+                            
+                            //cribblePacket stuff
+                            /*
+                            (cribblePacket.mouse).xcor = event.button.x;
+                            (cribblePacket.mouse).ycor = event.button.y;
+                            */
+                            
                             /*printf("%sxcor:%d\tycor:%d\n", tag, event.button.x, event.button.y);*/
                         }
                         break;
@@ -49,16 +56,38 @@ void getInput()
                 mouse.ycor = -1;
                 mouse.lastx = -1;
                 mouse.lasty = -1;
+                
+                //cribblePacket stuff
+                /*
+                (cribblePacket.mouse).lastx = -1;
+                (cribblePacket.mouse).lasty = -1;
+                (cribblePacket.mouse).xcor = -1;
+                (cribblePacket.mouse).ycor = -1;
+        	*/
+        	
                 break;
             case SDL_MOUSEMOTION:
                 if (SDL_GetMouseState(NULL, NULL)&SDL_BUTTON(1)) { //If mouse is down
                     mouse.xcor = event.button.x;
                     mouse.ycor = event.button.y;
+                    
+                    //cribblePacket stuff
+                    /*
+                    (cribblePacket.mouse).xcor = event.button.x;
+                    (cribblePacket.mouse).ycor = event.button.y;
+                    */
 
                     //added to deter segfaults in the vertical boundaries of the screen
                     if(mouse.ycor > 425 || mouse.ycor <= 2){
                         mouse.xcor = -1;
                         mouse.ycor = -1;
+                        
+                        //cribblePacket stuff
+                        /*
+                        (cribblePacket.mouse).xcor = -1;
+                        (cribblePacket.mouse).ycor = -1;
+                    	*/
+                    	
                     }
                     /*printf("%sxcor:%d\tycor:%d\n", tag, event.button.x, event.button.y);*/
                 }
