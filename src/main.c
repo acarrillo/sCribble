46879c925b19c6a7d6140439771ca81ce5a15cd0
azureity <don_vinny@yahoo.com>
2012-01-25 17:15:47 -0500

    Added what Doron asked me to add. It compiles. I don't understand the last bit: "send loopback address to client". I don't know if it works either; I don't know how to test this sort of stuff (server/client stuff).

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 /*
  * SDL framework borrowed from http://www.parallelrealities.co.uk/
  */
-
+#include <arpa/inet.h>
+#include <unistd.h>
 #include "main.h"
 
 extern void init(char *, char *);
@@ -10,9 +11,25 @@ extern void updateScreen(void);
 extern void getInput(void);
 
 extern void run_client(); // From client.c
+extern void initClient(char* addr);
 
 int main(int argc, char *argv[]) {
 
+
+
+  if(argc == 2){
+    if(inet_pton(AF_INET, argv[1], NULL)){
+      initClient(argv[1]); 
+    }
+    else
+      printf("Error occurred in second parameter. \n");
+  }
+  else if(argc == 1)
+    execlp("server", "server", NULL);
+  //send loopback address to client? how do?
+  
+  
+  
     init("sCribble", "127.0.0.1"); //Starts SDL, connects to 127.0.0.1
 
 
